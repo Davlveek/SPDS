7247827c69696fa5e8be1fcbee58018a9a8ad7be
davlveek <stankeev.vladimir@gmail.com>
2020-06-11 00:53:50 +0300

    spds main

--- spds.py
+++ spds.py
@@ -1,2 +1,13 @@
+from args import parse_arguments
+import ida
+import dynamorio
+
+
+def analysis(file):
+    #ida.analysis(file)
+    dynamorio.analysis(file)
+
+
 if __name__ == '__main__':
-    pass
+    args = parse_arguments()
+    analysis(args.file)
