8501f348e70c0acc14135da36ff3b268fac32fec
davlveek <stankeev.vladimir@gmail.com>
2020-06-11 00:52:10 +0300

    dynamorio agent

---
+++ dynagent.py
@@ -0,0 +1,24 @@
+from management.dynamorio.dynamorio_agent import DynamoRioAgent
+
+if __name__ == '__main__':
+    try:
+        agent = DynamoRioAgent()
+        agent.listen(1)
+        agent.accept_connection()
+
+        agent.recv_file(DynamoRioAgent.file)
+        print('File received from agency')
+
+        agent.start_dynamorio_analysis()
+        print('DynamoRIO analysis finished')
+
+        t = agent.form_dict_results()
+        print('Result parsing finished')
+
+        agent.send_results(t)
+        print('Results sended to agency')
+
+        agent.close_connection()
+        agent.close_socket()
+    except Exception as e:
+        print(f'Exception: {e}')
