98cce90f6223a77bf0255d6341eb6173f6d6c681
davlveek <stankeev.vladimir@gmail.com>
2020-06-11 00:52:26 +0300

    dynamorio analysis

--- dynamorio.py
+++ dynamorio.py
@@ -1,5 +1,19 @@
-from agency import Agency
+import json
+from management.dynamorio.dynamorio_agency import DynamoRioAgency
 
 
 def analysis(file):
-    pass
+    try:
+        agency = DynamoRioAgency(file)
+        agency.connect()
+        agency.send_file()
+        tricks = agency.recv_results()
+        with open('file1.txt', 'w') as f:
+            json.dump(tricks, f, sort_keys=False, indent=4)
+        agency.close()
+    except Exception as e:
+        print(f'Exception: {e}')
+
+
+if __name__ == '__main__':
+    analysis('file')
